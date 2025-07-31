0d2e96439e0bb7d7755e02cd6327509f8edd1f6a
Arun-Rajani <222484633+Arun-Rajani@users.noreply.github.com>
2025-07-31 12:44:50 +0400

    Reduced a line of code to calculate gross pay directly
    Instead of adding overtime pay with normal pay, calculated the gross pay directly to make the program more efficient and take less lines of code

--- Lesson-4-Conditional/gross-pay-calculator.py
+++ Lesson-4-Conditional/gross-pay-calculator.py
@@ -4,8 +4,7 @@ hours = float(hrs)
 if hours>40:
     normal_pay = 40 * float(rate)
     overtime_hrs = hours - 40
-    overtime_pay = overtime_hrs * 1.5 * float(rate)
-    gross_pay = overtime_pay + normal_pay
+    gross_pay = overtime_hrs * 1.5 * float(rate) + normal_pay
     print(gross_pay)
 else:
     normal_pay = float(rate) * hours
