661bd45fa9f93ef5b70bfb42b84abbe2c50c89ea
Arun-Rajani <222484633+Arun-Rajani@users.noreply.github.com>
2025-07-31 13:09:35 +0000

    Added type checking in first input value to save user time
    In the try block both inputs now have float function to convert data type so now the user will get an error message with just first input instead of 2 inputs saving user's time

--- Lesson-4-Conditional/pay-calculator-error-handling.py
+++ Lesson-4-Conditional/pay-calculator-error-handling.py
@@ -1,7 +1,7 @@
 try:
-    hrs = input("Enter hours:")
-    rate = input("Enter Rate:")
-    pay = float(hrs)*float(rate)
-    print("Pay:",pay)
+    hrs = float(input("Enter hours:"))
+    rate = float(input("Enter Rate:"))
+    pay = float(hrs) * float(rate)
+    print("Pay:", pay)
 except:
     print("Please enter numerical values")
